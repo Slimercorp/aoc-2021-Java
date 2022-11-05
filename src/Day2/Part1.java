f644fd36cf461083f54ff7ddd4d43238a8a6fa47
Sergei Chekmarev <slimercorp@gmail.com>
2022-11-05 13:02:15 +0400

    Day 2

---
+++ src/Day2/Part1.java
@@ -0,0 +1,57 @@
+package Day2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Part1 {
+
+	public static void main(String[] args) {
+		File file = new File(".\\src\\Day2\\data.txt");
+		Scanner scan;
+		
+		try {
+			 scan = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("File is not found");
+			return;
+		}
+		
+		List<String> data = new ArrayList<>();
+		
+		while (scan.hasNextLine()) {
+			data.add(scan.nextLine());
+		}
+		
+		int hor = 0;
+		int depth = 0;
+		for (int i=0; i<data.size(); i++) {
+			String[] splitted = data.get(i).split(" ");
+			
+			int value = Integer.parseInt(splitted[1]);
+			switch (splitted[0]) {
+				case "forward":
+					hor += value;
+					break;
+				case "down":
+					depth += value;
+					break;
+				case "up":
+					depth -= value;
+					break;
+				default:
+					System.out.println("error!");
+					return;
+			}
+					
+		}
+		
+		System.out.println(hor * depth);
+		
+		scan.close();
+	}
+
+}
